f4426996a27df70173f57596eea238b4854510dc
hengzhaoqing <hengzhaoqing@163.com>
2021-09-28 22:58:12 +0800

    初始化

---
+++ ice-orange-cloud/ice-orange-modules/ice-orange-modules-system/src/main/java/com/hzqing/modules/system/IceOrangeModulesSystemApplication.java
@@ -0,0 +1,16 @@
+package com.hzqing.modules.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hzq
+ */
+@SpringBootApplication
+public class IceOrangeModulesSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IceOrangeModulesSystemApplication.class, args);
+    }
+
+}
---
+++ ice-orange-cloud/ice-orange-modules/ice-orange-modules-system/src/test/java/com/hzqing/modules/system/IceOrangeModulesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzqing.modules.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceOrangeModulesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
