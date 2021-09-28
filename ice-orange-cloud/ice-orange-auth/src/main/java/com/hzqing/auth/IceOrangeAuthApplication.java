4858804b5086cda574962e80c682f33498d79465
hengzhaoqing <hengzhaoqing@163.com>
2021-09-28 23:15:01 +0800

    添加认证中心

---
+++ ice-orange-cloud/ice-orange-auth/src/main/java/com/hzqing/auth/IceOrangeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hzqing.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceOrangeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IceOrangeAuthApplication.class, args);
+    }
+
+}
---
+++ ice-orange-cloud/ice-orange-auth/src/test/java/com/hzqing/auth/IceOrangeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzqing.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceOrangeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
